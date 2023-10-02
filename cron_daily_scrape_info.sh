5ea798049ac35e40b938ac89a84d0ed1474cc3fd
jonathanbonnaud <bonnjona@hotmail.fr>
2023-10-02 15:48:06 +0200

    chore: fix script

--- cron_daily_scrape_info.sh
+++ cron_daily_scrape_info.sh
@@ -8,10 +8,10 @@ export PYTHONPATH=$PYTHONPATH:/Users/$USERNAME/PycharmProjects/bricklink-notion-
 
 for cat in sw sh; do
     echo $cat
-    python scraping/scrape_BL_minifig_info.py $cat >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_info_${TIMESTAMP}_std.log" 2>&1
+    python scraping/scrape_BL_minifig_info.py $cat >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_info_${TIMESTAMP}_std.log" 2>&1
 
     # upsert to Notion
-    python notion/async_upsert_minifigs_data.py $cat >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/upsert_to_notion_${TIMESTAMP}_std.log" 2>&1
+    python notion/async_upsert_minifigs_data.py $cat >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/upsert_to_notion_${TIMESTAMP}_std.log" 2>&1
 
     echo "Waiting 20 minutes..."
     sleep 1200
--- cron_weekly_scrape_init.sh
+++ cron_weekly_scrape_init.sh
@@ -4,24 +4,24 @@ TIMESTAMP=$(date +%F_%T)
 echo $TIMESTAMP
 
 source venv/bin/activate
-export PYTHONPATH=$PYTHONPATH:/Users/$USERNAME/PycharmProjects/bricklink-notion-integration
+export PYTHONPATH=$PYTHONPATH:/Users/$USER/PycharmProjects/bricklink-notion-integration
 
 type="sets"
 echo $type
 for cat in sw sh; do  # avt hp hfw col
     echo $cat
-    python scraping/scrape_BL_init.py $cat $type >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_init_${TIMESTAMP}_std.log" 2>&1
-    python scraping/scrape_BL_set_info.py $cat --scrape-only-release-year >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_info_${TIMESTAMP}_std.log" 2>&1
+    python scraping/scrape_BL_init.py $cat $type >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_init_${TIMESTAMP}_std.log" 2>&1
+    python scraping/scrape_BL_set_info.py $cat --scrape-only-release-year >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_info_${TIMESTAMP}_std.log" 2>&1
 
     # upsert to Notion
-    python notion/async_upsert_sets_data.py $cat >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/upsert_to_notion_${TIMESTAMP}_std.log" 2>&1
+    python notion/async_upsert_sets_data.py $cat >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/upsert_to_notion_${TIMESTAMP}_std.log" 2>&1
 done
 
 type="minifigs"
 echo $type
 for cat in sw sh; do  # avt hp hfw col
     echo $cat
-    python scraping/scrape_BL_init.py $cat $type >>"/Users/${USERNAME}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_init_${TIMESTAMP}_std.log" 2>&1
+    python scraping/scrape_BL_init.py $cat $type >>"/Users/${USER}/PycharmProjects/bricklink-notion-integration/cronlogs/scrape_BL_init_${TIMESTAMP}_std.log" 2>&1
 done
 
 deactivate
