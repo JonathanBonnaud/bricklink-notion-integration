25a11f01b5a3fc3362a8eed408fdaceb15360429
jonathanbonnaud <bonnjona@hotmail.fr>
2024-01-10 11:33:21 +0100

    chore: added private_secrets_TEMPLATE

---
+++ notion/private_secrets_TEMPLATE.py
@@ -0,0 +1,8 @@
+# First you need to create a Notion integration
+# https://www.notion.so/my-integrations
+
+# Private Notion secrets
+NOTION_USER_SECRET = ""  # Secret from Notion integration
+PAGE_ID = ""  # Page ID from Notion workspace
+
+DEFAULT_USER_PREFIX = "ME"  # Simple prefix for the user in the database
